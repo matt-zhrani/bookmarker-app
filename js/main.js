5261c008e52fb9d9bc2df0b2ce8be1f811ee15ff
matt-zhrani <ahmed.mm.alzhrani@gmail.com>
2017-01-17 00:59:58 -0600

    added indentation between Visit & Delete buttons

--- js/main.js
+++ js/main.js
@@ -71,8 +71,8 @@ function fetchBookmark(){
     var url= bookmarks[i].url;
     bookmarksResults.innerHTML += '<div class="well">' +
                                   '<h3>' + name +
-                                  '<a class="btn btn-default" target="_blank" href="'+url+'">Visit</a>'+
-                                  '<a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a>'+
+                                  ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> '+
+                                  ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a>'+
                                   '</h3></div>';
   }
 }
